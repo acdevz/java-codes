97938998c3c70cc9f8a0d66e80cf8f662da2e078
acdevs <amanchandra.in@gmail.com>
2023-04-03 18:42:59 +0530

    leetcode Updates.

--- src/main/java/leetcode/P7_ReverseInteger.java
+++ src/main/java/leetcode/P7_ReverseInteger.java
@@ -10,6 +10,7 @@ public class P7_ReverseInteger {
         int rev = 0;
         while (x != 0) {
             int pop = x % 10;
+            //check range before operation...
             if (rev > Integer.MAX_VALUE / 10 || (rev == Integer.MAX_VALUE / 10 && pop > 7)) return 0;
             if (rev < Integer.MIN_VALUE / 10 || (rev == Integer.MIN_VALUE / 10 && pop < -8)) return 0;
             rev = rev * 10 + pop;
