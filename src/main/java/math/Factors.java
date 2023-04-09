1e0a9822d05aed4593093ef5427ee7354234cc2d
acdevs <amanchandra.in@gmail.com>
2023-04-09 18:37:50 +0530

    factors. modulo. updates.

--- src/main/java/math/Factors.java
+++ src/main/java/math/Factors.java
@@ -1,9 +1,39 @@
 package math;
 
+import java.util.ArrayList;
+
 public class Factors {
     public static void main(String[] args) {
-
+//        factor(36);
+        System.out.println(gcd(8, 2));
+        System.out.println(lcm(6, 2));
     }
-    // Factors of a number!! but optimised?
 
+    /* Time & Space Complexity: O( sqrt(n) ) */
+    static void factor(int n){
+        ArrayList<Integer> factors = new ArrayList<>();
+        for (int i = 1; i * i <= n; i++) {
+            if(n % i == 0) {
+                System.out.print(i + " ");
+                if (n / i == i) continue;
+                factors.add(n / i);
+            }
+        }
+        for (int i = factors.size() - 1; i >= 0; i--) {
+            System.out.print(factors.get(i) + " ");
+        }
+    }
+
+    /* GCD : Basic Euclidean algorithm */
+    static int gcd(int a, int b){
+        if(a == 0) return b;
+        return gcd(b % a, a);
+    }
+
+    /* GCD : Extended Euclidean algorithm */
+
+    /* LCM */
+    static int lcm(int a, int b){
+        return (a * b) / gcd(a, b);
+    }
 }
---
+++ src/main/java/math/Modulo.java
@@ -0,0 +1,10 @@
+package math;
+
+public class Modulo {
+    /* Properties of modulo...
+    * (a + b) % m  =  ((a % m) + (b % m)) % m
+    * (a - b) % m  =  ((a % m) - (b % m) + m) % m
+    * (a * b) % m  =  ((a % m) * (b % m)) % m
+    * (a / b) % m  =  ((a % m) * ((1 / b) % m)) % m
+    */
+}
